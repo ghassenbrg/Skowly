fcc94cec7f44f59dc558a85f6dedb6b36777dadd
Ghassen Bargougui <ghassen.brg16@gmail.com>
2023-09-03 20:39:53 +0100

    change main Class package

--- eureka-server/src/main/java/com/skowly/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/skowly/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.skowly.eurekaserver;
+package com.skowly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- gateway/src/main/java/com/skowly/GatewayApplication.java
+++ gateway/src/main/java/com/skowly/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.skowly.gateway;
+package com.skowly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
