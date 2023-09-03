85617b9c2c6afe7c3da454f16f0fc18d709ff840
Ghassen Bargougui <ghassen.brg16@gmail.com>
2023-09-03 21:34:39 +0100

    [core] fix 403 error

--- skowly-core/src/main/java/com/skowly/config/SecurityConfig.java
+++ skowly-core/src/main/java/com/skowly/config/SecurityConfig.java
@@ -1,9 +1,15 @@
 package com.skowly.config;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,17 +18,19 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.authorizeHttpRequests(
-				registry -> registry.requestMatchers("/actuator/**").permitAll().anyRequest().authenticated());
-		/*
-		 * .oauth2ResourceServer(oauth2Configurer -> oauth2Configurer .jwt(jwtConfigurer
-		 * -> jwtConfigurer.jwtAuthenticationConverter(jwt -> { Map<String,
-		 * Collection<String>> realmAccess = jwt.getClaim("realm_access");
-		 * Collection<String> roles = realmAccess.get("roles"); var grantedAuthorities =
-		 * roles.stream() .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
-		 * .collect(Collectors.toList()); return new JwtAuthenticationToken(jwt,
-		 * grantedAuthorities); })));
-		 */
+		httpSecurity
+				.authorizeHttpRequests(registry -> 
+				registry.requestMatchers("/actuator/**").permitAll()
+						.anyRequest().authenticated())
+				.oauth2ResourceServer(oauth2Configurer -> oauth2Configurer
+						.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwt -> {
+							Map<String, Collection<String>> realmAccess = jwt.getClaim("realm_access");
+							Collection<String> roles = realmAccess.get("roles");
+							var grantedAuthorities = roles.stream()
+									.map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+									.collect(Collectors.toList());
+							return new JwtAuthenticationToken(jwt, grantedAuthorities);
+						})));
 
 		return httpSecurity.build();
 	}
