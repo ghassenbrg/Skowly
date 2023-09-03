4ec55983e8bbc55d32dd93d699e3787d21260e31
Ghassen Bargougui <ghassen.brg16@gmail.com>
2023-09-03 14:22:56 +0100

    keycloak instance + some fixes

--- build.bat
+++ build.bat
@@ -1,7 +1,57 @@
 @echo off
-mvn clean install -DskipTests & ^
-docker build -t eureka-server:1.0.0-SNAPSHOT -f ./eureka-server/Dockerfile ./eureka-server & ^
-docker build -t gateway:1.0.0-SNAPSHOT -f ./gateway/Dockerfile ./gateway & ^
-docker build -t config-server:1.0.0-SNAPSHOT -f ./config-server/Dockerfile ./config-server & ^
-docker build -t skowly-core:1.0.0-SNAPSHOT -f ./skowly-core/Dockerfile ./skowly-core & ^
-docker build -t skowly-ui:1.0.0-SNAPSHOT -f ./skowly-ui/Dockerfile ./skowly-ui
+setlocal enabledelayedexpansion
+
+set "SKIP_DOCKER=0"
+set "PROJECT=all"
+
+:loop
+if "%~1"=="" goto endloop
+if "%~1"=="-skipDocker" (set "SKIP_DOCKER=1") & (shift) & (goto loop)
+if "%~1"=="-sd" (set "SKIP_DOCKER=1") & (shift) & (goto loop)
+set "PROJECT=%~1"
+shift
+goto loop
+
+:endloop
+
+if "!PROJECT!"=="all" (
+    echo Building all projects...
+) else (
+    echo Building only !PROJECT!...
+)
+
+if !SKIP_DOCKER!==0 (
+    if "!PROJECT!"=="all" (
+        (
+            mvn clean install -DskipTests
+            docker build -t eureka-server:1.0.0-SNAPSHOT -f .\eureka-server\Dockerfile .\eureka-server
+            docker build -t gateway:1.0.0-SNAPSHOT -f .\gateway\Dockerfile .\gateway
+            docker build -t config-server:1.0.0-SNAPSHOT -f .\config-server\Dockerfile .\config-server
+            docker build -t skowly-core:1.0.0-SNAPSHOT -f .\skowly-core\Dockerfile .\skowly-core
+            docker build -t skowly-ui:1.0.0-SNAPSHOT -f .\skowly-ui\Dockerfile .\skowly-ui
+        )
+    ) else (
+        if not "!PROJECT!"=="skowly-ui" (
+            pushd .\!PROJECT!
+            mvn clean install -DskipTests
+            popd
+        )
+        docker build -t %PROJECT%:1.0.0-SNAPSHOT -f .\%PROJECT%\Dockerfile .\%PROJECT%
+    )
+) else (
+    if "!PROJECT!"=="all" (
+        (
+            mvn clean install -DskipTests
+            echo Skipping docker builds...
+        )
+    ) else (
+        if not "!PROJECT!"=="skowly-ui" (
+            pushd .\!PROJECT!
+            (
+                mvn clean install -DskipTests
+                echo Skipping docker builds...
+            )
+            popd
+        )
+    )
+)
