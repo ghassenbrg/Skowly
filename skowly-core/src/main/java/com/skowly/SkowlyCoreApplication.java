f452d4be84bbedd473862cf20e921ca97505b00e
Med Dhia <32268384+dhiasaadlaui@users.noreply.github.com>
2023-09-04 20:37:03 +0100

    Update SkowlyCoreApplication.java

--- skowly-core/src/main/java/com/skowly/SkowlyCoreApplication.java
+++ skowly-core/src/main/java/com/skowly/SkowlyCoreApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class SkowlyCoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SkowlyCoreApplication.class, args);
+		SpringApplication.run(SkowlyCoreApplication.class, args); 
 	}
 
 }
