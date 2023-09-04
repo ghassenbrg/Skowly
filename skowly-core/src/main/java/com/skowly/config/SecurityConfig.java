aacbe3d39ffb9568733f0ce71ce7ace5f365dde7
Ghassen Bargougui <ghassen.brg16@gmail.com>
2023-09-04 17:00:20 +0100

    EnableMethodSecurity

--- skowly-core/src/main/java/com/skowly/config/SecurityConfig.java
+++ skowly-core/src/main/java/com/skowly/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -14,14 +15,15 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity
-				.authorizeHttpRequests(registry -> 
-				registry.requestMatchers("/actuator/**").permitAll()
-						.anyRequest().authenticated())
+		.authorizeHttpRequests(registry -> 
+		registry.requestMatchers("/actuator/**").permitAll()
+				.anyRequest().authenticated())
 				.oauth2ResourceServer(oauth2Configurer -> oauth2Configurer
 						.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwt -> {
 							Map<String, Collection<String>> realmAccess = jwt.getClaim("realm_access");
--- skowly-core/src/main/java/com/skowly/controller/HelloWorldController.java
+++ skowly-core/src/main/java/com/skowly/controller/HelloWorldController.java
@@ -1,14 +1,13 @@
 package com.skowly.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("hello")
-    public String sayHello() {
-        return "Hello, World!";
-    }
+	@GetMapping("hello")
+	public String sayHello() {
+		return "Hello, World!";
+	}
 }
