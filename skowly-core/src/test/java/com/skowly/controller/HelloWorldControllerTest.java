7ac9b651b491af5dda74c260268cf88aacfb06ad
Med Dhia <32268384+dhiasaadlaui@users.noreply.github.com>
2023-09-05 12:38:51 +0100

    Create HelloWorldControllerTest.java

---
+++ skowly-core/src/test/java/com/skowly/controller/HelloWorldControllerTest.java
@@ -0,0 +1,29 @@
+package com.skowly.controller;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(HelloWorldController.class)
+public class HelloWorldControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testSayHello() throws Exception {
+        // Arrange
+        String expectedResponse = "Hello, World!";
+
+        // Act and Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+               .andExpect(MockMvcResultMatchers.status().isOk())
+               .andExpect(MockMvcResultMatchers.content().string(expectedResponse));
+    }
+}
