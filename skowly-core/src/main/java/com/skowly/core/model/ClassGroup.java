47f4df30236448b3506178fdf1c07520c350a73c
Karim BEN SAAD <kbensaad@vermeg.com>
2023-09-11 17:03:59 +0100

    add id to classGroup entity

--- skowly-core/src/main/java/com/skowly/core/model/ClassGroup.java
+++ skowly-core/src/main/java/com/skowly/core/model/ClassGroup.java
@@ -3,12 +3,19 @@ package com.skowly.core.model;
 import java.util.List;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Entity
 @Data public class ClassGroup {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
 	private String groupName;
 	
