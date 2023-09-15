dc316286f3fae3d2eed1996c0b7e0edf0bfa3e29
unknown <dhiasaadlaui@gmail.com>
2023-09-16 00:35:43 +0100

    removed test files angular

--- skowly-ui/src/app/core/core.module.ts
+++ skowly-ui/src/app/core/core.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { DashboardPageComponent } from './components/dashboard-page/dashboard-page.component';
-import { HeaderComponent } from './components/header/header.component';
+
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [SidebarComponent, HeaderComponent, DashboardPageComponent],
+  declarations: [SidebarComponent, DashboardPageComponent],
   imports: [CommonModule, RouterModule, BrowserModule, FormsModule],
   exports: [DashboardPageComponent],
 })
