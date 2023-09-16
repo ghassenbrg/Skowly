7db46d95e8aaef8501de8ddceaf1e734d52ab7e2
Ghassen Bargougui <ghassen.brg16@gmail.com>
2023-09-16 01:14:59 +0100

    header UI

---
+++ skowly-ui/src/app/core/components/header/header.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {
+  isFullScreen: boolean = false;
+
+  ngOnInit() {
+    this.isFullScreen = document.fullscreenElement ? true : false;
+  }
+
+  toggleFullscreen() {
+    if (!document.fullscreenElement) {
+      this.openFullscreen();
+      this.isFullScreen = true;
+    } else {
+      this.closeFullscreen();
+      this.isFullScreen = false;
+    }
+  }
+
+  openFullscreen() {
+    if ((document.documentElement as any).requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if ((document.documentElement as any).mozRequestFullScreen) {
+      /* Firefox */
+      (document.documentElement as any).mozRequestFullScreen();
+    } else if ((document.documentElement as any).webkitRequestFullscreen) {
+      /* Chrome, Safari & Opera */
+      (document.documentElement as any).webkitRequestFullscreen();
+    } else if ((document.documentElement as any).msRequestFullscreen) {
+      /* IE/Edge */
+      (document.documentElement as any).msRequestFullscreen();
+    }
+  }
+
+  closeFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if ((document as any).mozCancelFullScreen) {
+      /* Firefox */
+      (document as any).mozCancelFullScreen();
+    } else if ((document as any).webkitExitFullscreen) {
+      /* Chrome, Safari & Opera */
+      (document as any).webkitExitFullscreen();
+    } else if ((document as any).msExitFullscreen) {
+      /* IE/Edge */
+      (document as any).msExitFullscreen();
+    }
+  }
+}
