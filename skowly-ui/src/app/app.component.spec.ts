82ee78c4a28fa516292c05bf5284f2e44ad9c92c
Ghassen Bargougui <ghassen.brg16@gmail.com>
2023-09-18 00:08:28 +0100

    fix CI/CD + some css

--- skowly-ui/src/app/app.component.spec.ts
+++ skowly-ui/src/app/app.component.spec.ts
@@ -1,11 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LanguageService } from './core/services/language.service';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [
+      RouterTestingModule,
+      TranslateModule.forRoot()  // add this line
+    ],
+    declarations: [AppComponent],
+    providers: [TranslateService, LanguageService]  // add this line
   }));
 
   it('should create the app', () => {
@@ -13,5 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
 });
